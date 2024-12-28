d67324be17196a526d5e1338c3264226f5376c6a
Krzysztof <konarskikrzysztof1@gmail.com>
2024-12-29 00:44:32 +0100

    Added offer api

--- Models/Offer.cs
+++ Models/Offer.cs
@@ -10,6 +10,7 @@ namespace DrogeriaProjekt.Models
         public int OfferId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImagePath { get; set; }
         public float Discount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; } 
--- Services/Models/Interfaces/IOfferService.cs
+++ Services/Models/Interfaces/IOfferService.cs
@@ -4,9 +4,9 @@ namespace DrogeriaProjekt.Services.Models.Interfaces
 {
     public interface IOfferService
     {
-        public Task<ICollection<Offer>> GetAllOffersAsync();
-        public Task<ICollection<Product>> GetAllProductsAsync(Offer offer);
         public Task<bool> AddOfferAsync(Offer offer);
+        public Task<List<Offer>> GetAllOffersAsync();
+        public Task<List<Product>> GetAllProductsAsync(Offer offer);
         public Task<bool> UpdateOfferAsync(Offer offer);
         public Task<bool> RemoveOfferAsync(Offer offer);
         public Task<TimeSpan> GetOfferTimeLeftAsync(Offer offer);
